adf97066b69a68f1d64cd9a83ddca44681d8aaa5
Rafael <rafaelpianaro@gmail.com>
2021-11-30 00:12:12 -0300

    *home *bg

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import image from "../images/DSC_0147.jpg"
 
 export default function Home(){
-    return <h1>Home page</h1>
+    return (
+        <main>
+            <img src={image} alt="flower" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Aloha, I'm Rafael.</h1>
+            </section>
+        </main>
+    )
 }
