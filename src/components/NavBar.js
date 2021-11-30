d4c86cf66665bb302a49d14f8c591200ef120b24
Rafael <rafaelpianaro@gmail.com>
2021-11-30 00:11:42 -0300

    *home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,7 @@ export default function NavBar(){
         <header className="bg-red-600">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
-                    <NavLink to="/" activeclassname="text-white" className="text-white inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                    <NavLink to="/" activeClassName="text-white" className="text-white inline-flex items-center py-6 px-6 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Rafael
                     </NavLink>
                     <NavLink to="/post" activeClassName="text-red-100 bg-red-700"
