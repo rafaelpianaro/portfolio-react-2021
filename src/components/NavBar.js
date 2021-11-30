423f522a6644ca8bb238c2a7eecb6c4c44c9d445
Rafael <rafaelpianaro@gmail.com>
2021-11-29 23:14:46 -0300

    *overwrite NavLink to put styles

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+
+function NavLink({ to, className, activeClassName, inactiveClassName, ...rest}){
+    let location = useLocation()
+    let isActive = location.pathname === to
+    let allClassNames = className + (isActive ? `${activeClassName}` : `${inactiveClassName}`)
+    return <Link className={allClassNames} to={to} {...rest} />
+}
+
+export default function NavBar(){
+    
+    return (
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to="/" activeclassname="text-white" className="text-white inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                        Rafael
+                    </NavLink>
+                    <NavLink to="/post" activeClassName="text-red-100 bg-red-700"
+                            // inactiveClassName="text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                            inactiveClassName="text-red-200 hover:text-green-800"
+                            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        Blog Posts
+                    </NavLink>
+                    <NavLink to="/project" activeClassName="text-red-100 bg-red-700"
+                            // inactiveClassName="text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                            inactiveClassName="text-red-200 hover:text-green-800"
+                            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        Project
+                    </NavLink>
+                    <NavLink to="/about" activeClassName="text-red-100 bg-red-700"
+                            // inactiveClassName="text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                            inactiveClassName="text-red-200 hover:text-green-800"
+                            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800">
+                        About me
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
